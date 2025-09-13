ff3e9c650f870c61c13bba20dc64b80b643e55b8
Johan Gutierrez <johan030803@gmail.com>
2025-09-12 19:12:16 -0600

    Actualizado

--- 02-condicionales/05-none.py
+++ 02-condicionales/05-none.py
@@ -1,5 +1,15 @@
 
 
 
-print(bool(None))
+print(bool(None)) # False
 # Null == None
+# Nada, vacío, ausencia, sin existencia
+
+
+user = "Johan" # Esto es un Truthy, así que debe ser un True por defecto
+if user:
+    print("Usuario registrado") # Marcará esto
+else:
+    print("Nombre de usuario disponible")
+    
+
